d618ede1e43568c03e382f0c052fcf81b5c61ff8
opsappcsteam <132971504+opsappcsteam@users.noreply.github.com>
2023-05-22 11:21:48 +0800

    Update onboarding.py

--- onboarding.py
+++ onboarding.py
@@ -1,6 +1,11 @@
 try:
-    import mcns
+    from shared_functions import get_excel_name
+    print(f'Excel Detected: {get_excel_name()}')
+    env = input('What environment are you onboarding to? (sit/prod) : ')
+    from mcns import mcns_onboarding
+    mcns_onboarding(env)
     import mds
-    import sft
+    from sft import sft_onboarding
+    sft_onboarding(env)
 except ValueError:
     print('No Excel File Found!')
