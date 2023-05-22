2e9023f479c748d6e6f692aa05d9c3b3269202c0
opsappcsteam <132971504+opsappcsteam@users.noreply.github.com>
2023-05-22 14:00:40 +0800

    Update onboarding.py

--- onboarding.py
+++ onboarding.py
@@ -6,6 +6,7 @@ excel_name = get_excel_name()
 if excel_name != None:
     print(f'Excel Detected: {get_excel_name()}')
     env = input('What environment are you onboarding to? (sit/prod): ')
+    print('Ensure input is in lowercase!')
     mcns_onboarding(env)
     import mds
     sft_onboarding(env)
