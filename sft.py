20560ddd209d8a4c24a4185eb23327aa8c37f627
opsappcsteam <132971504+opsappcsteam@users.noreply.github.com>
2023-05-22 11:19:41 +0800

    Update sft.py

--- sft.py
+++ sft.py
@@ -2,13 +2,14 @@ import pandas as pd
 import shared_functions as sf
 import sft_functions as sfs
 
-app_name = sf.get_excel_name()
-excelsheet_info = pd.read_excel(sf.get_excel_path(), 'SFT')
+def sft_onboarding(env):
+  app_name = sf.get_excel_name()
+  excelsheet_info = pd.read_excel(sf.get_excel_path(), 'SFT')
 
-sns_callback_array = sfs.sft_array(excelsheet_info, 'SNS')
-http_callback_array = sfs.sft_array(excelsheet_info, 'HTTP')
-file_type_array = list(set(sfs.sft_array(excelsheet_info, 'File Type')))
-file_type_array = list(filter(lambda x: x != '(Select)', file_type_array))
+  sns_callback_array = sfs.sft_array(excelsheet_info, 'SNS')
+  http_callback_array = sfs.sft_array(excelsheet_info, 'HTTP')
+  file_type_array = list(set(sfs.sft_array(excelsheet_info, 'File Type')))
+  file_type_array = list(filter(lambda x: x != '(Select)', file_type_array))
 
-sft_authorizer_content = sfs.sft_auth_json(app_name, file_type_array, sns_callback_array, http_callback_array, 'prod')
-sft_auth_json = sf.file_generation(app_name, 'sft_authorizer', sft_authorizer_content, '.json')
+  sft_authorizer_content = sfs.sft_auth_json(app_name, file_type_array, sns_callback_array, http_callback_array, env)
+  sft_auth_json = sf.file_generation(app_name, 'sft_authorizer', sft_authorizer_content, '.json')
