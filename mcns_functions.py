b5d136eefd7c53c1eae1dca42fd47d4588763f19
opsappcsteam <132971504+opsappcsteam@users.noreply.github.com>
2023-05-26 13:01:58 +0800

    Update mcns_functions.py

--- mcns_functions.py
+++ mcns_functions.py
@@ -52,7 +52,8 @@ def mcns_array(df, value, env):
                     output_str = re.sub(r'\$\((\w+)\)', r'${\1}', input_str)
                     output_str = output_str.replace('"${', '${').replace('}"', '}').split("\n")
                     output_str = ' '.join(line.lstrip() for line in output_str)
-                    output_str = output_str.replace('"', '\\"')
+                    output_str = repr(output_str).replace(r'\t', '    ')
+                    output_str = output_str.replace(r'"', r'\"').replace("'", '"')[1:-1]
                     array.append(output_str)
             elif row[i] == value and value == "Template Values' Regular Expression":
                 if str(row[i + 2]) == 'nan':
