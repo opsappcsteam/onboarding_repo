0c58d4573bb098f8e292228bfaf244111b5727f5
opsappcsteam <132971504+opsappcsteam@users.noreply.github.com>
2023-05-30 11:22:43 +0800

    Update onboarding.py

--- onboarding.py
+++ onboarding.py
@@ -8,8 +8,9 @@ if excel_name != None:
     print(f'Excel Detected: {get_excel_name()}')
     try:
         env = input('What environment are you onboarding to? (sit/prod): ').lower()
+        version = input('Which onboarding version are you using? (1 / 2): ')
+        mds_onboarding(version)
         mcns_onboarding(env)
-        mds_onboarding()
         sft_onboarding(env)
     except IndexError:
         print('Enter a valid environment name; Run the program again!')
