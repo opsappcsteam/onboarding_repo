c87fe73e5adad2c27101b58811228fb4b06429cb
opsappcsteam <132971504+opsappcsteam@users.noreply.github.com>
2023-06-03 12:32:52 +0800

    Update onboarding.py

--- onboarding.py
+++ onboarding.py
@@ -1,26 +1,47 @@
-from Services.shared_functions import get_excel_name
+import pandas as pd
+from Services.shared_functions import onboarding_error, get_excel_name, get_excel_path
 from Services.MCNS.mcns import mcns_onboarding
 from Services.MDS.mds import mds_onboarding
 from Services.SFT.sft import sft_onboarding
 
+print('''
+############################
+ AppCS Onboarding Programme
+############################
+''')    
+
+env_array = ['sit', 'prod']
+version_array = ['1', '2']
 excel_name = get_excel_name()
-if excel_name != None:
-    print(f'Excel Detected: {get_excel_name()}')
-    env_array = ['sit', 'prod']
-    version_array = ['1', '2']
-    env = input('What environment are you onboarding to? (sit/prod): ').lower()
-    if env in env_array:
-        try:
-            version = input('Which onboarding version are you using? (1 / 2): ')
-            if version in version_array:
-                mds_onboarding(version)
-                mcns_onboarding(env)
-                sft_onboarding(env)
-            else:
-                print('The version you selected is invalid; Run the program again!')
-        except ValueError:
-            print('The version you selected is incorrect; Run the program again!')
-    else:
-        print('The environment you selected is incorrect; Run the program again!')
-else:
-    print('No Excel Detected')
+
+if excel_name == None:
+    onboarding_error('No Excel')
+
+print(f'Excel Detected: {get_excel_name()}')
+env = input('What environment are you onboarding to? (sit/prod): ').lower()
+
+if env not in env_array:
+    onboarding_error('Invalid Env')
+
+try:
+    version = input('Which onboarding version are you using? (1 / 2): ')
+    if version not in version_array:
+        onboarding_error('Invalid Version')
+
+    if version == '1':
+        pd.read_excel(get_excel_path(), 'MDS')
+    elif version == '2':
+        pd.read_excel(get_excel_path(), 'MPDS')
+
+    print(f'''
+############################
+Onboarding Environment: {env.upper()}
+Form Version: {version}
+############################
+    ''')    
+    mds_onboarding(version)
+    mcns_onboarding(env)
+    sft_onboarding(env)
+    print()
+except ValueError:
+    onboarding_error('Incorrect Version')
