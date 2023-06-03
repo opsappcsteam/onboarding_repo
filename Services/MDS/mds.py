77f71a3ffbef5e6653485023146b838c820e6b44
opsappcsteam <132971504+opsappcsteam@users.noreply.github.com>
2023-06-03 17:47:46 +0800

    Update mds.py

--- Services/MDS/mds.py
+++ Services/MDS/mds.py
@@ -8,7 +8,8 @@ def mds_onboarding(version):
         excelsheet_info = pd.read_excel(sf.get_excel_path(), 'MDS')
     elif version == '2':
         excelsheet_info = pd.read_excel(sf.get_excel_path(), 'MPDS')
-        
+
+    pop_svc_array = mf.mds_array(excelsheet_info, True)
     template_id_array = mf.mds_array(excelsheet_info, 'Template ID')
     template_array = mf.mds_array(excelsheet_info, 'Template')
     template_regex_array = mf.dynamic_values_array_of_arrays(template_array)
@@ -19,6 +20,6 @@ def mds_onboarding(version):
     field_array_of_arrays = mf.field_array_of_arrays(table_name_column_array, field_column_array)
 
     query_template_content = mf.mds_query_template_yaml(template_array, template_id_array, app_name, template_regex_array)
-    data_contract_content = mf.data_contract_yaml(app_name, table_names_array, field_array_of_arrays)
+    data_contract_content = mf.data_contract_yaml(app_name, table_names_array, field_array_of_arrays, pop_svc_array)
     query_template_yaml = sf.file_generation(app_name, 'mds_query_template', query_template_content, '.yaml')
     data_contract_yaml = sf.file_generation(app_name, 'mds_data_contract', data_contract_content, '.yaml')
