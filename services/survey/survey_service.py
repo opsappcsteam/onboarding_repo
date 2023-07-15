045821695c9b9bf1cefa9adc4718e803d7065b0f
opsappcsteam <132971504+opsappcsteam@users.noreply.github.com>
2023-07-15 15:20:17 +0800

    Update survey_service.py

--- services/survey/survey_service.py
+++ services/survey/survey_service.py
@@ -1,58 +1,35 @@
-from openpyxl import Workbook
-
 def onboard_csv_content(operation_array, project_name, role_array, email_array):
-    if 'onboard' in operation_array:
-        workbook = Workbook()
-        sheet1 = workbook.active
-        sheet1.title = 'Onboard'
-        sheet1.append(['operation', 'username', 'projectName', 'role', 'email'])
-        sheet1.append(['ONBOARD', project_name, project_name, 'System User', 'DF_ECS@dsta.gov.sg'])
+    if 'Onboard' in operation_array:
+        onboard_content = 'operation, username, projectName, role, email'
         i = 0
         while i < len(operation_array):
-            if operation_array[i] == 'onboard':
-                sheet1.append(['ONBOARD', email_array[i], project_name, role_array[i], email_array[i]])
-            else:
-                pass
+            if operation_array[i] == 'Onboard':
+                onboard_content += f'''
+ONBOARD, {email_array[i]}, {project_name}, Survey {role_array[i]}, {email_array[i]}'''
             i += 1
-        workbook.save(f'./artifacts/onboarding/{project_name}-Survey_Onboard.csv')
-        print('Survey Onboard: Generated')
-    else:
-        print('Survey Onboard: Skipped')
+        return onboard_content
+    return None
 
 def reset_csv_content(operation_array, project_name, role_array, email_array):
-    if 'reset' in operation_array:
-        workbook = Workbook()
-        sheet1 = workbook.active
-        sheet1.title = 'Reset'
-        sheet1.append(['operation', 'username', 'projectName', 'role', 'email'])
-
+    if 'Reset' in operation_array:
+        reset_content = 'operation, username, projectName, role, email'
         i = 0
         while i < len(operation_array):
-            if operation_array[i] == 'reset':
-                sheet1.append(['RESET', email_array[i], project_name, role_array[i], email_array[i]])
-            else:
-                pass
+            if operation_array[i] == 'Reset':
+                reset_content += f'''
+RESET, {email_array[i]}, {project_name}, Survey {role_array[i]}, {email_array[i]}'''
             i += 1
-        workbook.save(f'./artifacts/onboarding/{project_name}-Survey_Reset.csv')
-        print('Survey Reset: Generated')
-    else:
-        print('Survey Reset: Skipped')
+        return reset_content
+    return None
 
 def unlock_csv_content(operation_array, project_name, role_array, email_array):
-    if 'unlock' in operation_array:
-        workbook = Workbook()
-        sheet1 = workbook.active
-        sheet1.title = 'Unlock'
-        sheet1.append(['operation', 'username', 'projectName', 'role', 'email'])
-
+    if 'Unlock' in operation_array:
+        unlock_content = 'operation, username, projectName, role, email'
         i = 0
         while i < len(operation_array):
-            if operation_array[i] == 'unlock':
-                sheet1.append(['UNLOCK', email_array[i], project_name, role_array[i], email_array[i]])
-            else:
-                pass
+            if operation_array[i] == 'Unlock':
+                unlock_content += f'''
+UNLOCK, {email_array[i]}, {project_name}, Survey {role_array[i]}, {email_array[i]}'''
             i += 1
-        workbook.save(f'./artifacts/onboarding/{project_name}-Survey_Unlock.csv')
-        print('Survey Unlock: Generated')
-    else:
-        print('Survey Unlock: Skipped')
+        return unlock_content
+    return None
