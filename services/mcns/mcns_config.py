5cb9d5938104ab7ece3648002068483ce593aa70
opsappcsteam <132971504+opsappcsteam@users.noreply.github.com>
2023-07-19 14:39:00 +0800

    Update mcns_config.py

--- services/mcns/mcns_config.py
+++ services/mcns/mcns_config.py
@@ -1,69 +1,201 @@
-import re
+import unittest
 
-def mcns_configuration_entry(app_name, template_id, channel_type, sender, env):
-    if channel_type == 'email':
-        email_pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-        valid_email = re.findall(email_pattern, sender)
-        if valid_email ==  [] and env == 'sit':
-            sender = sender + '@sit.df-mcns.com'
-        elif valid_email == [] and env == 'prod':
-            sender = sender + '@mcns.defence.gov.sg'
-        sender = sender.lower()
+from services.mcns.mcns_config import *
 
-    if channel_type == 'sms':
-        if env == 'sit':
-            origination_number = '+6580283091'
-        if env == 'prod':
-            origination_number = '73884'
-        if sender == 'nan':
-            sender == origination_number   
-
-    mcns_config_entry = f'''
-        {{
-            "PutRequest": {{
-                "Item": {{
-                    "pk": {{
-                        "S": "{app_name}#{sender}#{template_id}"
-                    }},
-                    "sk": {{
-                        "S": "{channel_type}"
-                    }},
-                    "appid": {{
+class TestMcnsConfig(unittest.TestCase):
+    maxDiff = None
+    def test_mcns_configuration_entry(self):
+        app_name = 'app-test'
+        template_id = 'templateId'
+        channel_type = 'sms'
+        sender = 'testSender'
+        env = 'sit'
+        entry = mcns_configuration_entry(app_name, template_id, channel_type, sender, env)
+        expected_entry = '''
+        {
+            "PutRequest": {
+                "Item": {
+                    "pk": {
+                        "S": "app-test#testsender#templateId"
+                    },
+                    "sk": {
+                        "S": "sms"
+                    },
+                    "appid": {
                         "S": "<placeholder>"
-                    }}'''
-    if channel_type == 'sms':
-        origination_number_json = f''',
-                    "originationNumber": {{
-                        "S": "{origination_number}"
-                    }}'''
-        mcns_config_entry += origination_number_json
-    mcns_config_footer = f'''
-                }}
-            }}
-        }},'''
-    mcns_config_entry += mcns_config_footer
-    return mcns_config_entry
+                    },
+                    "originationNumber": {
+                        "S": "+6580283091"
+                    }
+                }
+            }
+        },'''
+        self.assertEqual(entry, expected_entry)
 
-def mcns_configuration_content(app_name, template_id_array, channel_type_array, sender_array, template_array, env):
-    content = f'''{{
-    "mcns-configuration-{env}": [ '''
-    content_footer = '''
+        app_name = 'app-test'
+        template_id = 'templateId'
+        channel_type = 'sms'
+        sender = 'nan'
+        env = 'prod'
+        entry = mcns_configuration_entry(app_name, template_id, channel_type, sender, env)
+        expected_entry = '''
+        {
+            "PutRequest": {
+                "Item": {
+                    "pk": {
+                        "S": "app-test#MINDEF#templateId"
+                    },
+                    "sk": {
+                        "S": "sms"
+                    },
+                    "appid": {
+                        "S": "<placeholder>"
+                    },
+                    "originationNumber": {
+                        "S": "73884"
+                    }
+                }
+            }
+        },'''
+        self.assertEqual(entry, expected_entry)
+
+        app_name = 'app-test'
+        template_id = 'templateId'
+        channel_type = 'sms'
+        sender = 'testSender'
+        env = 'prod'
+        entry = mcns_configuration_entry(app_name, template_id, channel_type, sender, env)
+        expected_entry = '''
+        {
+            "PutRequest": {
+                "Item": {
+                    "pk": {
+                        "S": "app-test#testSender#templateId"
+                    },
+                    "sk": {
+                        "S": "sms"
+                    },
+                    "appid": {
+                        "S": "<placeholder>"
+                    },
+                    "originationNumber": {
+                        "S": "73884"
+                    }
+                }
+            }
+        },'''
+        self.assertEqual(entry, expected_entry)
+
+        app_name = 'app-test'
+        template_id = 'templateId'
+        channel_type = 'email'
+        sender = 'testSender'
+        env = 'sit'
+        entry = mcns_configuration_entry(app_name, template_id, channel_type, sender, env)
+        expected_entry = '''
+        {
+            "PutRequest": {
+                "Item": {
+                    "pk": {
+                        "S": "app-test#testsender@sit.df-mcns.com#templateId"
+                    },
+                    "sk": {
+                        "S": "email"
+                    },
+                    "appid": {
+                        "S": "<placeholder>"
+                    }
+                }
+            }
+        },'''
+        self.assertEqual(entry, expected_entry)
+
+    def test_mcns_configuration_content(self):
+        app_name = 'app-test'
+        template_id_array = ['templateId1', 'templateId2'] 
+        channel_type_array = ['sms', 'email']
+        sender_array = ['testSms', 'testEmail']
+        template_array = ['template1', 'template2']
+        env  = 'prod'
+        content = mcns_configuration_content(app_name, template_id_array, channel_type_array, sender_array, template_array, env)
+        expected_content = '''{
+    "mcns-configuration-prod": [ 
+        {
+            "PutRequest": {
+                "Item": {
+                    "pk": {
+                        "S": "app-test#testSms#templateId1"
+                    },
+                    "sk": {
+                        "S": "sms"
+                    },
+                    "appid": {
+                        "S": "<placeholder>"
+                    },
+                    "originationNumber": {
+                        "S": "73884"
+                    }
+                }
+            }
+        },
+        {
+            "PutRequest": {
+                "Item": {
+                    "pk": {
+                        "S": "app-test#testemail@mcns.defence.gov.sg#templateId2"
+                    },
+                    "sk": {
+                        "S": "email"
+                    },
+                    "appid": {
+                        "S": "<placeholder>"
+                    }
+                }
+            }
+        }
     ]
 }'''
+        self.assertEqual(content, expected_content)
 
-    i = 0
-    while i < len(template_array):
-        if template_array[i] != 'nan':
-            config_entry = mcns_configuration_entry(app_name, template_id_array[i], channel_type_array[i], sender_array[i], env)
-            content += config_entry
-        i += 1
-
-    content = content[:-1]
-    content += content_footer
-
-    if content != f'''{{
-    "mcns-configuration-{env}": [
+        app_name = 'app-test'
+        template_id_array = ['templateId1', 'nan'] 
+        channel_type_array = ['sms', 'nan']
+        sender_array = ['testSms', 'nan']
+        template_array = ['template1', 'nan']
+        env  = 'prod'
+        content = mcns_configuration_content(app_name, template_id_array, channel_type_array, sender_array, template_array, env)
+        expected_content = '''{
+    "mcns-configuration-prod": [ 
+        {
+            "PutRequest": {
+                "Item": {
+                    "pk": {
+                        "S": "app-test#testSms#templateId1"
+                    },
+                    "sk": {
+                        "S": "sms"
+                    },
+                    "appid": {
+                        "S": "<placeholder>"
+                    },
+                    "originationNumber": {
+                        "S": "73884"
+                    }
+                }
+            }
+        }
     ]
-}}''':
-        return content
-    return None
+}'''
+        self.assertEqual(content, expected_content)
+
+        app_name = 'app-test'
+        template_id_array = ['templateId1', 'nan'] 
+        channel_type_array = ['sms', 'nan']
+        sender_array = ['testSms', 'nan']
+        template_array = ['nan', 'nan']
+        env  = 'prod'
+        content = mcns_configuration_content(app_name, template_id_array, channel_type_array, sender_array, template_array, env)
+        self.assertIsNone(content)
+
+if __name__ == '__main__':
+    unittest.main()
