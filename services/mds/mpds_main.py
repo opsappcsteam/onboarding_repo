ee420567c0a1939a4bbc730f61a30329da627261
opsappcsteam <132971504+opsappcsteam@users.noreply.github.com>
2023-10-05 11:06:22 +0800

    Update mpds_main.py

--- services/mds/mpds_main.py
+++ services/mds/mpds_main.py
@@ -13,7 +13,7 @@ def mpds_main(excel_path, app_name):
     field_arrays = nan_remover(field_array_splitter(column_array(mpds_sheet, 'Table Name'), column_array(mpds_sheet, 'Field')))
     mpds_data_contract_content = mds_contract_content(app_name, pop_svc_array, table_name_array, field_arrays)
 
-    template_id_array = row_array(mpds_sheet, 'Template ID', 3)
+    template_id_array = uuid_generator(row_array(mpds_sheet, 'Template ID', 3))
     template_array = template_subject_cleaner(row_array(mpds_sheet, 'Template', 3), 'MPDS')
     regex_arrays = dynamic_values_array_generator([], template_array)
     mpds_query_template_content = mds_template_content(app_name, template_array, template_id_array, regex_arrays)
