cc765361e9356cfd7ed23018d47069b96518500d
opsappcsteam <132971504+opsappcsteam@users.noreply.github.com>
2023-10-06 16:03:40 +0800

    Update file_generation.py

--- services/common/file_generation.py
+++ services/common/file_generation.py
@@ -8,7 +8,7 @@ def onboarding_file_generation(app_name, service, file_name, file_content, file_
         file_version = ''
         print_version = ''
     if file_content != None:
-        with open(f'./artifacts/onboarding/{app_name}-{service}_{file_file_name}{file_version}.txt', 'w', encoding='utf-8") as file:
+        with open(f'./artifacts/onboarding/{app_name}-{service}_{file_file_name}{file_version}.txt', 'w', encoding="utf-8") as file:
             file.write(file_content)
         txt_file = os.path.join('./artifacts/onboarding/', f'{app_name}-{service}_{file_file_name}{file_version}.txt')
         output_file = txt_file.replace('.txt', file_extension)
@@ -18,7 +18,7 @@ def onboarding_file_generation(app_name, service, file_name, file_content, file_
         print(f'{service} {file_name}{print_version}: Skipped')
 
 def reporting_file_generation(app_name, report_content):
-    with open(f'./artifacts/reports/{app_name}-report.txt', 'w', encoding='utf-8") as file:
+    with open(f'./artifacts/reports/{app_name}-report.txt', 'w', encoding="utf-8") as file:
         file.write(report_content)
     txt_file = os.path.join('./artifacts/reports/', f'{app_name}-report.txt')
     report_file = txt_file.replace('.txt', '.html')
