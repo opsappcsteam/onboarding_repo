90ecf0cd28b7a9ad3f50f2e76b9d06aa9d53f340
opsappcsteam <132971504+opsappcsteam@users.noreply.github.com>
2023-10-19 18:03:24 +0800

    Update array_manipulation.py

--- services/common/array_manipulation.py
+++ services/common/array_manipulation.py
@@ -12,20 +12,33 @@ def nan_remover(array):
         no_nan_array = list(filter(lambda x: x != 'nan', array))
     return no_nan_array
 
-def template_subject_cleaner(array, service):
+def template_subject_cleaner(array, service, channel_type_array):
     clean_template_array = []
-    for item in array:
-        item = re.sub(r'\$\((\w+)\)', r'${\1}', item)
+    i = 0
+    if channel_type_array == []:
         if service == 'MDS' or service == 'MPDS':
-            item = item.replace('"${', '${').replace('}"', '}').split("\n")
-            item = ' '.join(line.lstrip() for line in item)
-            item = re.sub(r'(\$\{.*?\}|".*?")|([a-zA-Z_]+)', lambda match: match.group(1) if match.group(1) else match.group(2).lower(), item)
-        elif service == 'MCNS':
-            item = item.replace('\t', '    ').replace('\\', '\\\\')
-            item = item.split("\n")
-            item = '\\n'.join(line.lstrip() for line in item)
-            item = item.replace("'", '"').replace(r'"', r'\"')
-        clean_template_array.append(item)
+            for item in array:
+                item = item.replace('"${', '${').replace('}"', '}').split("\n")
+                item = ' '.join(line.lstrip() for line in item)
+                item = re.sub(r'(\$\{.*?\}|".*?")|([a-zA-Z_]+)', lambda match: match.group(1) if match.group(1) else match.group(2).lower(), item)
+                clean_template_array.append(item)
+    else:
+        while i < len(array):
+            if channel_type_array[i] == 'email' or channel_type_array == 'push':
+                item = array[i]
+                item = item.replace('\t', '    ').replace('\\', '\\\\')
+                item = item.split("\n")
+                item = ' '.join(line.lstrip() for line in item)
+                item = item.replace("'", '"').replace(r'"', r'\"') 
+                clean_template_array.append(item)
+            else:
+                item = array[i]
+                item = item.replace('\t', '    ').replace('\\', '\\\\')
+                item = item.split("\n")
+                item = '\\n'.join(line.lstrip() for line in item)
+                item = item.replace("'", '"').replace(r'"', r'\"') 
+                clean_template_array.append(item)
+            i += 1
     return clean_template_array
 
 def dynamic_values_array_generator(subject_array, template_array):
